67285ba429526d8b57997146d257b44c59b5920b
GowthamR0 <gowtham.2001046@srec.ac.in>
2025-06-23 07:44:23 +0530

    removed

--- src/controllers/googlesignin.controller.ts
+++ src/controllers/googlesignin.controller.ts
@@ -7,8 +7,6 @@ import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 
 
 export const googleSignin = (req: Request, res: Response) => {
-  // client_ID='380142193554-4bl3riuhgg8uf31lfho7vj3m27ukdpv6.apps.googleusercontent.com'
-  // Client_SecretKey="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"
 //   const client_ID = process.env.client_ID!;
 //   const secretKey = process.env.secret_Key!;
 
