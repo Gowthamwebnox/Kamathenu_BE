60b8805ca6bea63027c414efc3e59f54659aeb4e
GowthamR0 <gowtham.2001046@srec.ac.in>
2025-06-23 07:30:57 +0530

    remove the secret code

--- src/controllers/googlesignin.controller.ts
+++ src/controllers/googlesignin.controller.ts
@@ -22,21 +22,21 @@ import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 export const googleSignin = (req: Request, res: Response) => {
   // client_ID='380142193554-4bl3riuhgg8uf31lfho7vj3m27ukdpv6.apps.googleusercontent.com'
   // Client_SecretKey="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"
-  const client_ID = process.env.client_ID!;
-  const secretKey = process.env.secret_Key!;
+//   const client_ID = process.env.client_ID!;
+//   const secretKey = process.env.secret_Key!;
 
-  passport.use(
-  new GoogleStrategy(
-    {
-      clientID: client_ID,
-      clientSecret: secretKey,
-      callbackURL: "http://localhost:8000/auth/google/callback",
-    },
-    (accessToken: any, refreshToken: any, profile: any, done: (arg0: null, arg1: any) => any) => {
-      return done(null, profile);
-    }
-  )
-);
+//   passport.use(
+//   new GoogleStrategy(
+//     {
+//       clientID: client_ID,
+//       clientSecret: secretKey,
+//       callbackURL: "http://localhost:8000/auth/google/callback",
+//     },
+//     (accessToken: any, refreshToken: any, profile: any, done: (arg0: null, arg1: any) => any) => {
+//       return done(null, profile);
+//     }
+//   )
+// );
 
   // Serialize user
   passport.serializeUser((user, done) => {
